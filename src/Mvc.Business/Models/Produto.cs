6e60b7bfb060a95c2e692174375c437c099e0657
Mateus Castellar <mateuscastellar@outlook.com>
2022-08-15 22:09:47 -0300

    feat: Models na camada de negocios

---
+++ src/Mvc.Business/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿namespace Mvc.Business.Models
+{
+    public class Produto : Entity
+    {
+        public Guid FornecedorId { get; set; }
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime Date { get; set; }
+        public bool Ativo { get; set; }
+
+        //EfCore Relacionamento
+        public Fornecedor Fornecedor { get; set; }
+    }
+}
---
+++ src/Mvc.Business/Models/TipoFornecedor.cs
@@ -0,0 +1,8 @@
+﻿namespace Mvc.Business.Models
+{
+    public enum TipoFornecedor
+    {
+        PessoaFisica = 1,
+        PessoaJuridica
+    }
+}
