7f7c3723e60755e0f5fc1fb52e5e13fd1a95ce1f
Mateus Castellar <mateuscastellar@outlook.com>
2022-08-15 22:44:45 -0300

    feat: Migrations e gerando base de dados

--- src/Mvc.Data/Context/AppContext.cs
+++ src/Mvc.Data/Context/AppContext.cs
@@ -3,12 +3,27 @@ using Mvc.Business.Models;
 
 namespace Mvc.Data.Context
 {
-    public class AppContext : DbContext
+    public class AppMvcContext : DbContext
     {
-        public AppContext(DbContextOptions<AppContext> context) : base(context) { }
+        public AppMvcContext(DbContextOptions<AppMvcContext> context) : base(context) { }
 
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Fornecedor> Fornecedores { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppMvcContext).Assembly);
+
+            //desabilitar cascade delete
+            foreach (var relacionamento in modelBuilder.Model
+                .GetEntityTypes()
+                .SelectMany(lbda => lbda.GetForeignKeys()))
+            {
+                relacionamento.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
