a35d50d524060bbc5a65e8ab1c615489e32969af
Mateus Castellar <mateuscastellar@outlook.com>
2022-08-17 09:57:11 -0300

    feat: Servico para persistir fornecedor com validacoes

--- src/Mvc.Business/Services/FornecedorService.cs
+++ src/Mvc.Business/Services/FornecedorService.cs
@@ -6,27 +6,71 @@ namespace Mvc.Business.Services
 {
     public class FornecedorService : BaseService, IFornecedorService
     {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IEnderecoRepository _enderecoRepository;
+
+        public FornecedorService(IFornecedorRepository fornecedorRepository, IEnderecoRepository enderecoRepository)
+        {
+            _fornecedorRepository = fornecedorRepository;
+            _enderecoRepository = enderecoRepository;
+        }
+
         public async Task Adicionar(Fornecedor fornecedor)
         {
-            if (ExecutarValidacao(new FornecedorValidation(), fornecedor) is false)
+            //validar estado do fornecedor e do seu endereco
+            if (ExecutarValidacao(new FornecedorValidation(), fornecedor) is false &&
+                ExecutarValidacao(new EnderecoValidation(), fornecedor.Endereco) is false)
+            {
                 return;
+            }
 
-            return;
+            if (_fornecedorRepository.Buscar(lbda =>
+                lbda.Documento == fornecedor.Documento).Result.Any())
+            {
+                Notificar("Já existe um fornecedor cadastrado com este documento");
+                return;
+            }
+
+            await _fornecedorRepository.Adicionar(fornecedor);
         }
 
-        public Task Atualizar(Fornecedor fornecedor)
+        public async Task Atualizar(Fornecedor fornecedor)
         {
-            throw new NotImplementedException();
+            //validar estado do fornecedor e do seu endereco
+            if (ExecutarValidacao(new FornecedorValidation(), fornecedor) is false)
+            {
+                return;
+            }
+
+            if (_fornecedorRepository.Buscar(lbda =>
+                lbda.Documento == fornecedor.Documento && lbda.Id != fornecedor.Id).Result.Any())
+            {
+                Notificar("Já existe um fornecedor cadastrado com este documento");
+                return;
+            }
+
+            await _fornecedorRepository.Atualizar(fornecedor);
         }
 
-        public Task AtualizarEndereco(Endereco endereco)
+        public async Task AtualizarEndereco(Endereco endereco)
         {
-            throw new NotImplementedException();
+            if (ExecutarValidacao(new EnderecoValidation(), endereco) is false)
+            {
+                return;
+            }
+
+            await _enderecoRepository.Atualizar(endereco);
         }
 
-        public Task Remover(Guid id)
+        public async Task Remover(Guid id)
         {
-            throw new NotImplementedException();
+            if (_fornecedorRepository.ObterFornecedorProdutosEndereco(id).Result.Produtos.Any())
+            {
+                Notificar("O Fornecedor possui produtos cadastrados");
+                return;
+            }
+
+            await _fornecedorRepository.Remover(id);
         }
     }
 }
