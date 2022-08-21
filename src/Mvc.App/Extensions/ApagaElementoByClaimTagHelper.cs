20960ab89e0960ab4255edf91db41d3dea4d8b04
Mateus Castellar <mateuscastellar@outlook.com>
2022-08-21 11:51:11 -0300

    feat: Ocultando elementos na view de acordo com Claims do usuario

---
+++ src/Mvc.App/Extensions/ApagaElementoByClaimTagHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Mvc.App.Extensions
+{
+    [HtmlTargetElement("*", Attributes = "supress-by-claim-name")]
+    [HtmlTargetElement("*", Attributes = "supress-by-claim-value")]
+    public class ApagaElementoByClaimTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public ApagaElementoByClaimTagHelper(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        [HtmlAttributeName("supress-by-claim-name")]
+        public string IdentityClaimName { get; set; }
+
+        [HtmlAttributeName("supress-by-claim-value")]
+        public string IdentityClaimValue { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context is null) throw new ArgumentNullException(nameof(context));
+            if (output is null) throw new ArgumentNullException(nameof(output));
+
+            var possuiAcesso = CustomAuthorization
+                .ValidarClaimsUsuario(_contextAccessor.HttpContext, IdentityClaimName, IdentityClaimValue);
+
+            if (possuiAcesso) return;
+
+            //Não gera o elemento html na tela(view)
+            output.SuppressOutput();
+        }
+    }
+}
